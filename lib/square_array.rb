b4fd747e9d2931a95d51203e0f87c7c985273802
Plimon Howard V <plimon.howard@gmail.com>
2020-01-09 21:20:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
+numbers = [1,2,3]
+
 def square_array(array)
-  # your code here
+     rtarray = Array.new(array.size)
+     count = 0
+     while array[count] do
+       puts count
+       rtarray[count] << array[count]**2
+       count += 1
+     end
+      return rtarray
 end
+
+square_array(numbers)
